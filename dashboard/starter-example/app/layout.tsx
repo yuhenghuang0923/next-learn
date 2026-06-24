d43978efaf8447609224dbabdd39061d93b13189
yuhenghuang0923 <yuhenghuang@ddmarketinghub.com>
2026-06-24 16:05:27 +0800

    fix: layout children type

--- dashboard/starter-example/app/layout.tsx
+++ dashboard/starter-example/app/layout.tsx
@@ -1,7 +1,11 @@
 import './ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
-export default function RootLayout({ children }: {}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>{children}</body>
